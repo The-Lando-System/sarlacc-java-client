5471ec9cdea2ea184631a4b6926c121fc58e371c
Matt <matt.voget@gmail.com>
2016-12-16 20:52:30 -0700

    using properties

--- src/main/java/com/mattvoget/sarlacc/client/authentication/SarlaccAuthProvider.java
+++ src/main/java/com/mattvoget/sarlacc/client/authentication/SarlaccAuthProvider.java
@@ -5,6 +5,7 @@ import com.mattvoget.sarlacc.models.Token;
 import com.mattvoget.sarlacc.models.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -14,7 +15,20 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class SarlaccAuthProvider implements AuthenticationProvider {
+
     private Logger log = LoggerFactory.getLogger(SarlaccAuthProvider.class);
+
+    @Value("${auth.url.token}")
+    private String authUrlToken;
+
+    @Value("${auth.url.user}")
+    private String authUrlUser;
+
+    @Value("${auth.client.id}")
+    private String authClientId;
+
+    @Value("${auth.client.password}")
+    private String authClientPassword;
 
     @Override
     public Authentication authenticate(Authentication authentication) throws AuthenticationException {
@@ -35,9 +49,9 @@ public class SarlaccAuthProvider implements AuthenticationProvider {
         String name = authentication.getName();
         String password = authentication.getCredentials().toString();
 
-        log.info(String.format("Calling Sarlacc to authenticate user with creds: username=%s, password=%s",name,password));
+        log.info(String.format("Calling Sarlacc to authenticate user with creds: username=%s, password=****",name));
 
-        SarlaccClient client = new SarlaccClient("acme","acmesecret","http://localhost:8080/oauth/token","http://localhost:8080/user-details");
+        SarlaccClient client = new SarlaccClient(authClientId,authClientPassword,authUrlToken,authUrlUser);
 
         Token token = client.getUserToken(name,password,"password");
         User user = client.getUserDetails(token);
